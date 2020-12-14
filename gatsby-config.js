cbac90a339cc23456af92e680299ccb2da22f560
Calvin Williams <calvin.william1012@gmail.com>
2020-12-13 22:20:35 -0500

    updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Songs of the Seas`,
+    description: `All your Sea Shanty needs, sea shanty lyrics, sea shanty history and other related music content.`,
+    author: `Calvin Williams`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

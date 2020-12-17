4a231e11fbeb09590afd8069485f2face941cc0e
Calvin Williams <calvin.william1012@gmail.com>
2020-12-17 01:40:04 -0500

    header component const to function

--- src/components/header.js
+++ src/components/header.js
@@ -3,53 +3,56 @@ import PropTypes from "prop-types"
 import React from "react"
 import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <header className="header-nav">
-      <h1 className="header-h1">
-        <Link
-        to="/"
-        className="header-button"
-        >
-          {siteTitle}
-        </Link>   
-      </h1>
-      <div className="header-button-wrapper">
-        <Link
-            to="/history"
-            className="header-button"
-          >
-            History
-        </Link>
+function Header ({ siteTitle }) {
 
-        <Link
-            to="/songs"
-            className="header-button"
-          >
-            Songs
-        </Link>
-        <Link
-            to="/about"
-            className="header-button"
-          >
-            About
-        </Link>
-        <Link
-            to="/blog"
-            className="header-button"
-          >
-            Blog
-        </Link>
 
-      </div>
+  return (
+    <header className="header-nav">
+        <h1 className="header-h1">
+          <Link
+          to="/"
+          className="header-button"
+          >
+            {siteTitle}
+          </Link>   
+        </h1>
+        <div className="header-button-wrapper">
+          <Link
+              to="/history"
+              className="header-button"
+            >
+              History
+          </Link>
 
-      <div className="header-hamburger">
-        <div className="header-hamburger-div"></div>
-        <div className="header-hamburger-div"></div>
-        <div className="header-hamburger-div"></div>
-      </div>
-  </header>
-)
+          <Link
+              to="/songs"
+              className="header-button"
+            >
+              Songs
+          </Link>
+          <Link
+              to="/about"
+              className="header-button"
+            >
+              About
+          </Link>
+          <Link
+              to="/blog"
+              className="header-button"
+            >
+              Blog
+          </Link>
 
+        </div>
+
+        <div className="header-hamburger">
+          <div className="header-hamburger-div"></div>
+          <div className="header-hamburger-div"></div>
+          <div className="header-hamburger-div"></div>
+        </div>
+    </header>
+  )
+}
 Header.propTypes = {
   siteTitle: PropTypes.string,
 }

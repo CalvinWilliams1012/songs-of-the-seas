17cdee71d5e7fd73562405f4638a02074abca1b4
Calvin Williams <calvin.william1012@gmail.com>
2020-12-26 19:52:13 -0500

    removed placeholder text

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,8 +7,6 @@ const BlogsPage = () => (
   <Layout>
     <SEO title="Blog" />
     <h1>Blog</h1>
-    <p>Welcome to Songs of the Seas</p>
-    <p>This site is still in development, come back in a few weeks to see the progress :)</p>
     <BlogRoll/>
   </Layout>
 )

696dd7b2544b84f4671edcd83ccce3cb5581940e
Calvin Williams <calvin.william1012@gmail.com>
2020-12-27 23:30:07 -0500

    Added a songroll and updated songs page

---
+++ src/components/songroll.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql, StaticQuery} from 'gatsby'
+
+
+class SongRoll extends React.Component {
+  render() {
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
+
+    return (
+      <div>
+        {posts &&
+          posts.map(({ node: post }) => (
+            <div key={post.id}>
+              <article>
+                <header>                    
+                  <Link
+                      to={post.fields.slug}
+                    >
+                  <p>
+                      {post.frontmatter.title}
+                  </p>
+                  </Link>
+                </header>
+              </article>
+            </div>
+          ))}
+      </div>
+    )
+  }
+}
+
+SongRoll.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query SongRollQuery {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___title] }
+          filter: { frontmatter: { template: { eq: "song" } } }
+        ) {
+          edges {
+            node {
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                template
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={(data, count) => <SongRoll data={data} count={count} />}
+  />
+)
--- src/pages/songs.js
+++ src/pages/songs.js
@@ -1,13 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import SongRoll from "../components/songroll"
 
 const SongsPage = () => (
   <Layout>
     <SEO title="Songs" />
     <h1>Songs</h1>
-    <p>Welcome to Songs of the Seas</p>
-    <p>This site is still in development, come back in a few weeks to see the progress :)</p>
+    <p>Index of songs in alphabetical order:</p>
+    <SongRoll/>
   </Layout>
 )
 

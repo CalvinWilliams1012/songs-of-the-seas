d4ceb61c85975985f52db8729b40382065f25da5
Calvin Williams <calvin.william1012@gmail.com>
2021-01-09 17:04:19 -0500

    Added image and small amount of content to the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,30 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from 'gatsby-image'
+import { graphql, Link } from "gatsby"
 
-const IndexPage = () => (
+
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to Songs of the Seas</p>
-    <p>This site is still in development, come back in a few weeks to see the progress :)</p>
+    <Img fluid={data.file.childImageSharp.fluid}/>
+    <h1>Welcome to Songs of the Seas</h1>
+    <p>To learn more about this site visit our <Link to='/about'>About page</Link>, or for more details on the history of the songs visit our <Link to='/history'>History page</Link>.</p>
   </Layout>
 )
+
+
+export const query = graphql`
+  {
+    file(relativePath: {eq: "sailboat-wide.jpg"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage

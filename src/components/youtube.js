0d8f0d392cf22ef309d8d5a7456750a34124e55b
Calvin Williams <calvin.william1012@gmail.com>
2021-01-16 17:14:09 -0500

    Added embeded youtube component

---
+++ src/components/youtube.js
@@ -0,0 +1,18 @@
+import React from "react"
+import PropTypes from 'prop-types'
+
+function YoutubeVideo({video}){  
+  return (
+      <div className="videoWrapper">
+        <iframe width="1680" height="690"  src={video} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+      </div>
+  )
+
+}
+
+YoutubeVideo.propTypes = {
+  video: PropTypes.string,
+}
+
+export default YoutubeVideo

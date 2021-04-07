8bc8b1b3a967aff490a9a08984a01117a5e5b8a1
Calvin Williams <calvin.william1012@gmail.com>
2021-04-06 22:37:45 -0400

    fixed warning where title was required for iframe

--- src/components/youtube.js
+++ src/components/youtube.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 function YoutubeVideo({video}){  
   return (
       <div className="videoWrapper">
-        <iframe width="1680" height="690"  src={video} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        <iframe title="youtube-video" width="1680" height="690"  src={video} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
         </iframe>
       </div>
   )
